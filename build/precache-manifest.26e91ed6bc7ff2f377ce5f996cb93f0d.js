c91df9c9e3e494060b62594ff7ad5b8a6a78c791
naor <naormordechaix@gmail.com>
2020-07-16 02:37:26 +0300

    new build

---
+++ build/precache-manifest.26e91ed6bc7ff2f377ce5f996cb93f0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5cc8bb9483327df64abcbf5a4d00ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e2b64206b88e4c31ac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8bd220bccfd3ee10ecda",
+    "url": "/static/js/2.d69d7c26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d69d7c26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e2b64206b88e4c31ac",
+    "url": "/static/js/main.053645e5.chunk.js"
+  },
+  {
+    "revision": "fe8f033a4a97e64a1331",
+    "url": "/static/js/runtime-main.37f68239.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
